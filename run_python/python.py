db3f6005f03f4343106a0ab70cf43aa78452de58
roll <roll@respect31.com>
2012-01-24 19:30:28 +0400

    first commit

---
+++ run_python/python.py
@@ -0,0 +1,27 @@
+import os
+import re
+import subprocess
+from run import BaseDriver, Settings
+from run.library.reader import Reader
+from run.library.property import cachedproperty
+
+class PythonDriver(BaseDriver):
+
+    def process(self):
+        return subprocess.call(self._rendered, shell=True)
+
+    @cachedproperty
+    def _rendered(self):
+        return (self._reader.read('command').
+                format(module=self._module,
+                       function=self.command.function,
+                       arguments=self.command.arguments))
+        
+    @cachedproperty
+    def _module(self):
+        return re.sub(Settings.LANGUAGS['python'], '', 
+                      self.command.filename)
+    
+    @cachedproperty
+    def _reader(self):
+        return Reader(os.path.dirname(__file__), 'templates')
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+from run import VERSION
+
+setup(name='run-python',
+      version=VERSION,
+      description='Run Python',
+      author='Respect31',
+      author_email='team@respect31.com',
+      maintainer='roll',
+      maintainer_email='roll@respect31.com',
+      url='https://github.com/respect31/run-python',
+      download_url='https://github.com/respect31/run-python/tarball/develop',
+      packages=['run_python',],      
+      classifiers=[],)
