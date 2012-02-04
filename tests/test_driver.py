f81195d3cc4220bcd353045e5eb2ceb938ca0047
roll <roll@respect31.com>
2012-02-04 14:31:33 +0400

    imp PythonDriver test

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -1,4 +1,34 @@
 import unittest
-
-class PythonDriverTest(unittest.TestCase):
-    pass
+import subprocess
+from run.library.patcher import Patcher
+from run_python import PythonDriver  
+     
+class PythonDriverTest(unittest.TestCase):       
+      
+    PATCH = {
+        'subprocess.call': lambda *args, **kwargs: kwargs['env']         
+    }
+      
+    def setUp(self):
+        self.patcher = Patcher(globals())
+        self.patcher.patch(self.PATCH)
+        self.command = CommandFixture() 
+        self.driver = PythonDriver(self.command)
+        
+    def tearDown(self):
+        self.patcher.restore()
+        
+    def test_process(self):
+        self.assertEqual(self.driver.process(), self.driver._environ)
+        
+    def test_environ(self):
+        self.assertIn('RUN_COMMAND', self.driver._environ)
+        self.assertEqual(self.driver._environ['RUN_COMMAND'], {})
+        
+    def test_connector(self):
+        self.assertIn('Connector', self.driver._connector)
+        
+                  
+class CommandFixture(object):
+    
+    json = {}             
