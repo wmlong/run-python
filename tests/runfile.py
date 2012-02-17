b52a718ddf5d9de07fd7418b26b79d47a67c9df8
roll <roll@respect31.com>
2012-02-17 11:14:04 +0400

    minor change

--- tests/runfile.py
+++ tests/runfile.py
@@ -1,11 +1,15 @@
 import sys
+
+#Test imports
 import subprocess
+from fixtures import Process
+subprocess, Process
 
 def function_normal(a, b='default', *args, **kwargs):
     """
     docstring
     """
-    sys.stdout.write('function_normal:'+str(a)+','+b+'|'+str(subprocess))
+    sys.stdout.write('function_normal: {a}, {b}'.format(a=a, b=b))
     
 def function_empty():
     pass 
