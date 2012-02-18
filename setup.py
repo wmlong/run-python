159d2757b6dadf2f2bdf16572c57df227bf94277
roll <roll@respect31.com>
2012-02-18 21:24:32 +0400

    updated setup

--- setup.py
+++ setup.py
@@ -1,41 +1,4 @@
-import run_python
-from setuptools import setup, find_packages
+from package import Package
+from setuptools import setup
 
-setup(#Data
-      name='run-python',
-      version=run_python.__version__,
-      packages=find_packages(exclude=['tests*']),
-      package_data={
-          'run_python': ['files/*'],
-      },      
-      install_requires=['run-core'],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      
-      #Metadata      
-      description='Python driver for Run',
-      long_description=open('README.rst').read(),
-      author='Respect31',
-      author_email='team@respect31.com',
-      maintainer='Respect31',
-      maintainer_email='team@respect31.com',
-      url='https://github.com/respect31/run-python',
-      download_url=('https://github.com/respect31/run-python/tarball/{version}'.
-                    format(version=run_python.__version__)),
-      license='MIT license',
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'Intended Audience :: System Administrators',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: Unix',
-          'Operating System :: POSIX',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2.7',
-          'Topic :: Software Development',
-          'Topic :: Software Development :: Libraries',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: System :: Systems Administration',
-          'Topic :: Utilities',  
-      ],)
+setup(**Package())
